e81eb4139cc52e7945803697455a26cd644dc30e
levantiendat <levan040669@gmail.com>
2025-04-08 16:24:27 +0700

    update time zone

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,18 +1,23 @@
 from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
-import datetime
+from datetime import datetime
+import pytz
 from app.core.security import verify_password
 
 Base = declarative_base()
+tz = pytz.timezone("Asia/Bangkok")  # GMT+7
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False)
-    role = Column(String(50), default="user")  # Các giá trị: admin, user, instructor
+    role = Column(String(50), default="user")
     email = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
-    created_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        default=lambda: datetime.now(tz)
+    )
     phone_number = Column(String(20), nullable=True)
 
     def check_password(self, plain_password: str) -> bool:
