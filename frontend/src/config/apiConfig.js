0ea4f5e6a6d9a31e626703bbf7c3f8606dea92de
levantiendat <levan040669@gmail.com>
2025-05-13 16:32:06 +0700

    update config for frontend

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,8 +1,8 @@
 // src/config/apiConfig.js
 
 const config = {
-    BASE_API_URL: 'https://university-admission-dut-hzdahmckevehhpdf.southeastasia-01.azurewebsites.net/api'
-  }
+  BASE_API_URL: process.env.VUE_APP_BASE_API_URL || 'https://university-admission-dut-hzdahmckevehhpdf.southeastasia-01.azurewebsites.net/api'
+}
   
   export default config
   
