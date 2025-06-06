61c6adcac0beed6d10d2e3b96750e15ec98ba5ae
levantiendat <levan040669@gmail.com>
2025-06-06 13:42:37 +0700

    feat: Update NLU and stories for admission methods and majors, and modify model path for improved functionality

--- Rasa_Chatbot/main.py
+++ Rasa_Chatbot/main.py
@@ -4,7 +4,7 @@ import time
 import socket
 import requests
 
-MODEL_PATH = "models/20250604-005450-terminal-tiger.tar.gz"
+MODEL_PATH = "models/20250606-084457-staccato-sickbay.tar.gz"
 NGROK_AUTH_TOKEN = "2vn6H02jcPXaZY8MnH7ziimHO0v_4eyF1uzWGLXHCCKod2ceG"
 NGROK_DOMAIN = "cockatoo-cheerful-factually.ngrok-free.app"
 RASA_PORT = 5005
