6d3e4f670c0f18380952c124ece67fb46bb2d9d0
levantiendat <levan040669@gmail.com>
2025-06-11 17:00:18 +0700

    feat: Update model path to the latest version for improved performance

--- Rasa_Chatbot/main.py
+++ Rasa_Chatbot/main.py
@@ -4,7 +4,7 @@ import time
 import socket
 import requests
 
-MODEL_PATH = "models/20250607-100140-plain-shoot.tar.gz"
+MODEL_PATH = "models/20250611-111208-windy-pocket.tar.gz"
 NGROK_AUTH_TOKEN = "2vn6H02jcPXaZY8MnH7ziimHO0v_4eyF1uzWGLXHCCKod2ceG"
 NGROK_DOMAIN = "cockatoo-cheerful-factually.ngrok-free.app"
 RASA_PORT = 5005
